4cf7dfb02cde643b2dd0c799a3c66f67b63365b9
Moses Nova <mosesnova@gmail.com>
2020-08-18 12:31:43 +1200

    filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,9 +3,15 @@ import React, { Component} from 'react'
 export default class Filter extends Component{
     render() {
         return(
-
-        <div>
-            <div className="filter-result">{this.props.count}</div>
+        <div className="filter">
+            <div className="filter-result">{this.props.count}Products </div>
+            <div className="filter-sort">
+                {" "}
+                Order <select>
+                    <option>Latest</option>
+                    <option value="lowest">Lowest</option>
+                </select>
+            </div>
             <div className="filter-size">
               Filter{" "}
               <select
